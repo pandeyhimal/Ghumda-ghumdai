2c1b182abdcefd517ab20e472e0fc7de590ac9d9
itsm22051@gmail.com <himalpandey2002@gmail.com>
2025-08-25 12:34:42 +0545

    Frontend routes managing main file

---
+++ client/src/App.tsx
@@ -0,0 +1,49 @@
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ContentProvider } from "@/contexts/ContentContext";
+import { LanguageProvider } from "@/contexts/LanguageContext";
+import Index from "./pages/Index";
+import About from "./pages/About";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import ContentDetail from "./pages/ContentDetail";
+import NotFound from "./pages/NotFound";
+import { Bookmarks } from "./pages/Bookmarks";
+import SocialLogin from "./components/SocialLogin";
+import { Scroll } from "lucide-react";
+import  {ScrollToTop}  from "../utils/ScrolltoTop";
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <LanguageProvider>
+        <ContentProvider>
+          <Toaster />
+          <Sonner />
+          <BrowserRouter>
+          <ScrollToTop />
+            <Routes>
+              <Route path="/" element={<Index />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/bookmarks" element={<Bookmarks />} />
+              <Route path="/content/:title" element={<ContentDetail />} />
+              {/* social login callback handler */}
+              <Route path="/auth/callback" element={<SocialLogin />} />
+              {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </BrowserRouter>
+        </ContentProvider>
+      </LanguageProvider>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
+
+export default App;
