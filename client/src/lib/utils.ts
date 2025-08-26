66b498471de7c9ce8ea3c36ed9ec9871710c9ac5
itsm22051@gmail.com <himalpandey2002@gmail.com>
2025-08-26 21:41:11 +0545

    Updates on URL routing utils

--- client/src/lib/utils.ts
+++ client/src/lib/utils.ts
@@ -4,3 +4,8 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+type ViteImportMeta = ImportMeta & { env?: Record<string, string | undefined> };
+const viteMeta: ViteImportMeta = (import.meta as unknown as ViteImportMeta) || ({} as ViteImportMeta);
+const rawBase = (viteMeta.env?.VITE_API_URL || "http://localhost:5000").replace(/\/$/, "");
+export const API_BASE: string = rawBase;
