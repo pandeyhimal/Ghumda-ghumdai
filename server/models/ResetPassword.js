104b4bf12d277e8c1be91b4522368a40d24004c9
itsm22051@gmail.com <himalpandey2002@gmail.com>
2025-08-28 13:39:51 +0545

    Password reset models

---
+++ server/models/ResetPassword.js
@@ -0,0 +1,10 @@
+// import mongoose from "mongoose";
+
+// const userSchema = new mongoose.Schema({
+//   email: { type: String, required: true, unique: true },
+//   password: { type: String, required: true },
+//   resetOTP: { type: String }, 
+//   resetOTPExpires: { type: Date } 
+// });
+
+// export default mongoose.model("ResetUser", userSchema);
